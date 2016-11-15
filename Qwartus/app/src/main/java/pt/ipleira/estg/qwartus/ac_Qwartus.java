59e80ba93b98cadf2514b08c1b300a69ecc66e0d
miguel12340 <miguelrochacalhau@hotmail.com>
2016-11-15 17:12:28 +0000

    1º versao
    by jacob

---
+++ Qwartus/app/src/main/java/pt/ipleira/estg/qwartus/ac_Qwartus.java
@@ -0,0 +1,13 @@
+package pt.ipleira.estg.qwartus;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ac_Qwartus extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ac__qwartus);
+    }
+}
